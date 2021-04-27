f339c952583d18ac4be70ab9ea467fd0e77bcb1c
Jamyth Luk <jamythluk@Jamyths-MacBook-Pro.local>
2021-04-27 19:13:43 +0800

    [FEATURE]: web: update build script

--- script/build.ts
+++ script/build.ts
@@ -1,15 +1,7 @@
-import { spawn } from './spawn';
-import { createConsoleLogger } from '@iamyth/logger';
+import { WebpackBuilder } from '@iamyth/webpack-runner';
 import path from 'path';
 
-const logger = createConsoleLogger('TypeScript Compiler');
-
-require('./format');
-require('./lint');
-
-function build() {
-    logger.task('Build and Transpile');
-    spawn('tsc', ['--project', path.join(__dirname, '../config/tsconfig.src.json')], 'Build Failed.');
-}
-
-build();
+new WebpackBuilder({
+    projectDirectory: path.join(__dirname, '..'),
+    tsconfigFilePath: path.join(__dirname, '../config/tsconfig.src.json'),
+}).run();
