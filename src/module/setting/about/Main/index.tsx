0cd6c24b376cfeb179aceb8583fd44493d626750
Jamyth Luk <jamythluk@jamyth.local>
2021-04-30 18:33:06 +0800

    [REFACTOR]: web: update version

--- src/module/setting/about/Main/index.tsx
+++ src/module/setting/about/Main/index.tsx
@@ -9,7 +9,7 @@ import type { State as SettingDistrictState } from 'module/setting/district/type
 import { Restaurant } from 'type/restaurant';
 import { Flex, useColorModeValue, Image, Text } from '@chakra-ui/react';
 
-const VERSION = 'Version 1.0.3';
+const VERSION = 'Version 1.0.4';
 
 export const Main = React.memo(() => {
     const allLocations = MTRUtil.getAllLocation();
