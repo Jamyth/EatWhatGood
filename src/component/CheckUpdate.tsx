23458d27e8037a59ae29f6618e7ec65806887add
Jamyth Luk <jamythluk@jamyth.local>
2021-05-05 16:05:27 +0800

    [REFACTOR]: web: update SW

--- src/component/CheckUpdate.tsx
+++ src/component/CheckUpdate.tsx
@@ -4,18 +4,37 @@ import { useToast } from '@chakra-ui/react';
 export const CheckUpdate = React.memo(() => {
     const toast = useToast();
 
+    const handleUpdate = async () => {
+        if ('serviceWorker' in navigator) {
+            let refreshing: boolean;
+            const oldSw = (await navigator.serviceWorker.getRegistration())?.active?.state;
+            navigator.serviceWorker.addEventListener('controllerchange', async () => {
+                if (refreshing) return;
+
+                // when the controllerchange event has fired, we get the new service worker
+                const newSw = (await navigator.serviceWorker.getRegistration())?.active?.state;
+
+                // if there was already an old activated service worker, and a new activating service worker, do the reload
+                if (oldSw === 'activated' && newSw === 'activating') {
+                    refreshing = true;
+                    toast({
+                        description: '有新版本喇喎！將自動更新',
+                        position: 'bottom',
+                        isClosable: true,
+                        status: 'info',
+                        duration: 3000,
+                    });
+                    setTimeout(() => {
+                        window.location.reload();
+                    }, 3000);
+                }
+            });
+        }
+    };
+
     React.useEffect(() => {
-        (window as any)?.['isUpdateAvailable'].then((isAvailable: boolean) => {
-            if (isAvailable) {
-                toast({
-                    description: '有新版本喇喎！',
-                    position: 'bottom',
-                    isClosable: true,
-                    status: 'info',
-                });
-            }
-        });
-    });
+        handleUpdate();
+    }, []);
 
     return null;
 });
